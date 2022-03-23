6dbcc76fc2d931e680c65583ad455d6323905f12
qdqd <33158502+qd-qd@users.noreply.github.com>
2022-03-23 17:14:34 +0100

    feat: sign typed data using wagmi
    - Fix the version of ethers as recommended in the documentation because
    the functionnality to signed typed data is subject to change (https://docs.ethers.io/v5/api/signer/#Signer-signTypedData)
    - Refactor the handleSubmit function
    - Rework a bit the HTML structure of the page

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,68 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
-import { ethers } from "ethers";
 import useAccount from "../src/utils/useAccount";
 import { domain, types } from "../src/utils/EIP712";
+import { useSignTypedData } from "wagmi";
 
 // @dev: Import the component client-side only (no SSR because metamask requires window)
 const Connector = dynamic(() => import("../src/components/Connector"), {
   ssr: false,
 });
-// TODO: remove me to use signed message function from wagmi
-const ethersWallet = new ethers.Wallet(process.env.NEXT_PUBLIC_USER_PRIV_KEY);
 
 function Test() {
-  const [voucherID, setVoucherID] = useState("");
+  const [tokenId, setTokenId] = useState("");
   const [{ data: account }] = useAccount();
+  const [, signTypedData] = useSignTypedData();
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    // The data to sign
-    const value = { owner: account.address, tokenId: voucherID };
+    const value = { owner: account.address, tokenId };
 
-    // TODO: use signed message function from wagmi
-    const signature = await ethersWallet._signTypedData(domain, types, value);
-    const data = { value, signature };
-    fetch("api/check", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const res = await response.text();
-    console.log(res);
+    try {
+      const signature = await signTypedData({ domain, types, value });
+
+      if (signature.error) throw signature.error;
+
+      const response = await fetch("api/check", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ value, signature: signature.data }),
+      });
+
+      const data = await response.json();
+      // TODO: Display status in the UI
+      console.log(data.ok);
+    } catch (e) {
+      throw e;
+    }
   }
 
   return (
-    <div className="Example">
-      <Connector />
-      <h1>Redeem Voucher</h1>
-      <div>
-        <p>Account is {account?.address}</p>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Enter voucher ID to redeem:
-            <input
-              type="number"
-              value={voucherID}
-              onChange={(e) => setVoucherID(e.target.value)}
-            />
-          </label>
-          <input type="submit" disabled={!voucherID} />
-        </form>
-      </div>
-    </div>
+    <>
+      <header>
+        <h1>Redeem Voucher</h1>
+        <Connector />
+      </header>
+      <main>
+        <section>
+          <p>Account is {account?.address}</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Enter voucher ID to redeem:
+              <input
+                type="number"
+                value={tokenId}
+                onChange={(e) => setTokenId(e.target.value)}
+              />
+            </label>
+            <input type="submit" disabled={!tokenId} />
+          </form>
+        </section>
+      </main>
+    </>
   );
 }
 
