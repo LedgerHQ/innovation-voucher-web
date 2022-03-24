e20738cf1623fc3aea341c8c1bcd8c98a18d5833
qdqd <33158502+qd-qd@users.noreply.github.com>
2022-03-24 18:31:50 +0100

    feat: set backend cache-control to 1 day

--- pages/api/nfts/[owner].ts
+++ pages/api/nfts/[owner].ts
@@ -1,11 +1,12 @@
 import api from "../../../src/utils/api";
 
-// TODO: Manage cache control
 const getAllNFTs = async ({ query: { owner }, method }, res) => {
   if (method !== "GET") return res.status(404);
 
   try {
     const nts = await api.getNFTs(owner);
+    // @dev: set a cache of 1 day
+    res.setHeader("Cache-Control", "s-maxage=86400");
     res.status(200).json({ data: nts, error: null });
   } catch (e) {
     res.status(500).json({ data: null, error: e.message });
