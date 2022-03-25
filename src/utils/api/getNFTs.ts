9d9781b5c495d8cd0699e901cdbdf7d1e7742e98
qdqd <33158502+qd-qd@users.noreply.github.com>
2022-03-25 20:52:32 +0100

    chore: add comments to getNFTs route

--- src/utils/api/getNFTs.ts
+++ src/utils/api/getNFTs.ts
@@ -1,12 +1,16 @@
-import { Nft } from "@alch/alchemy-web3";
+import { Nft as NftType } from "@alch/alchemy-web3";
 
-const network = process.env.NODE_ENV === "development" ? "polygon-mumbai" : "polygon-mainnet";
-const baseUrl = `https://${network}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}/getNFTs/`;
-// @dev: The request is filtered using the smart contract address
-const endpoint = `${baseUrl}?withMetadata=true&contractAddresses[]=${process.env.NEXT_PUBLIC_TYPEDDATADOMAIN_VOUCHER_CONTRACT}`;
+// @dev: Networks are hardcoded here. If we want to support other networks, we should
+//       map suppported networks with network name used by the Alchemy API
+//       (https://docs.alchemy.com/alchemy/apis/polygon-api#mainnet-vs.-testnet)
+const network = process.env.NODE_ENV === "development" ? "eth-kovan" : "polygon-mainnet";
+const baseUrl = `https://${network}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
+// @dev: The request is filtered using our smart contract address
+const params = `withMetadata=true&contractAddresses[]=${process.env.NEXT_PUBLIC_TYPEDDATADOMAIN_VOUCHER_CONTRACT}`;
+const endpoint = `${baseUrl}/getNFTs/?${params}`;
 
-type FetchNFTs = (owner: string, nfts?: Array<Nft>, pageKey?: string) => Promise<Nft[]>;
-type JSONResponse = { ownedNfts: Array<Nft>; pageKey: string };
+type FetchNFTs = (owner: string, nfts?: Array<NftType>, pageKey?: string) => Promise<NftType[]>;
+type JSONResponse = { ownedNfts: Array<NftType>; pageKey: string };
 
 const getNFTs: FetchNFTs = async (owner, nfts = [], pageKey = null) => {
   // @dev: the API crashes if we pass an pageKey equals to 0 or null, only use this param if needed
@@ -15,11 +19,9 @@ const getNFTs: FetchNFTs = async (owner, nfts = [], pageKey = null) => {
 
   const data: JSONResponse = await response.json();
   const ownedNFTs = [...nfts, ...data.ownedNfts];
-  /*
-   ** @dev: pageKey is non-null if there are more pages to fetch
-   ** That's why this function is a recursive function, we continue to
-   ** call it until we have all the NFTs owned by the owner
-   */
+  // @dev: Each request returns up to 100 NFTs. pageKey is non-null if there
+  //       are more pages to fetch. That's why this function is a recursive function,
+  //       we continue to call it until we have all the NFTs owned by the owner
   return data.pageKey ? await getNFTs(owner, ownedNFTs, data.pageKey) : ownedNFTs;
 };
 
