6b26fac4467e67a4c38aeb64fa9213bc949a10c7
qdqd <qdqdqdqdqd@protonmail.com>
2022-04-07 17:35:41 +0200

    chore: install/setup `@ledgerhq/react-ui` package
    - Create custom `_document` to manage styled-component server side rendering
    - Override default Nextjs babel config to manage styled-components
    - Add the UI provider to the `_app` component and set the theme as dark
    - Create global styles to set the background color of the dark mode
    - Add all the fonts needed by the package
    - Add these library
    --- @ledgerhq/react-ui
    --- styled-components
    --- babel-plugin-styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import WAGMIProvider from "../src/utils/WAGMIProvider";
+import { StyleProvider } from "@ledgerhq/react-ui";
+import "../public/style.css";
 
+/*
+ ** Next.js uses the App component to initialize pages. You can override it and control the page initialization.
+ ** We need it to inject our providers on each pages.
+ */
 const CustomApp = ({ Component, pageProps }: AppProps) => (
-  <WAGMIProvider>
-    <Component {...pageProps} />
-  </WAGMIProvider>
+  <StyleProvider selectedPalette="dark" fontsPath="/fonts">
+    <WAGMIProvider>
+      <Component {...pageProps} />
+    </WAGMIProvider>
+  </StyleProvider>
 );
 
 export default CustomApp;
